cdabc4d5c88afb4e0012f26cea671e17e975304e
Medhat Elmasry <medhat@elmasry.ca>
2021-01-14 20:03:36 -0800

    1st code commit

---
+++ Models/Helper.cs
@@ -0,0 +1,83 @@
+using System;
+using System.IO;
+
+namespace UnzipLHub.Models
+{
+    public class Helper
+    {
+        public static void Log(string logMessage, TextWriter w)
+        {
+            w.Write("\r\nLog Entry : ");
+            w.WriteLine("{0} {1}", System.DateTime.Now.ToLongTimeString(),
+                DateTime.Now.ToLongDateString());
+            w.WriteLine("  :");
+            w.WriteLine("  :{0}", logMessage);
+            w.WriteLine("-------------------------------");
+        }
+
+        public static int DeleteAllFilesInFolder(string path)
+        {
+            try
+            {
+                System.IO.DirectoryInfo di = new DirectoryInfo(path);
+
+                int count = 0;
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                    count++;
+                }
+
+                return count;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine($"Directory {path} could not be found.");
+                return -1;
+            }
+        }
+
+        public static void Log(string msg)
+        {
+            string filename = DateTime.Now.ToString("yyyyMMdd") + "_log.txt";
+            using (StreamWriter w = File.AppendText(filename))
+            {
+                Log(msg, w);
+                Log(msg, w);
+            }
+        }
+
+        public static string GetLastNameBefore(string filename, int index)
+        {
+            int startLastName = filename.Substring(0, index).LastIndexOf("_");
+            return filename.Substring(startLastName + 1, index - startLastName);
+        }
+
+        public static int GetMonthIndex(string filename)
+        {
+            int index = -1;
+            string[] months = { "Jan", "Feb", "Mar", "Apr", "May",
+                "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+            foreach (var mnth in months)
+            {
+                if (filename.IndexOf("_" + mnth + " ") > -1)
+                {
+                    index = filename.IndexOf("_" + mnth);
+                    break;
+                }
+            }
+
+            return index;
+        }
+
+        public static void CopyFileToTarget(string source, string targetPath) {
+                string filename = Path.GetFileName(source);
+                string sourcePath = Path.GetDirectoryName(source);
+
+                Console.WriteLine("Copying {0}", filename);
+                string targetFilename = source.Replace(sourcePath, targetPath);
+                File.Copy(source, targetFilename);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using UnzipLHub.Models;
+
+namespace UnzipLHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IConfiguration Configuration = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+              .AddEnvironmentVariables()
+              .AddCommandLine(args)
+              .Build();
+
+            var sourcePath = Configuration["Directories:SourcePath"];
+            var targetPath = Configuration["Directories:TargetPath"];
+            var sortBy = Configuration["Sorting:SortBy"];
+            var deleteFilesInTargetDirectory = Configuration["Files:DeleteFilesInTargetDirectory"];
+
+            Console.WriteLine("UnzipLHub version 1.0.0");
+
+            string[] zipFiles = Directory.GetFiles(sourcePath, "*.zip");
+
+            if (deleteFilesInTargetDirectory != null && deleteFilesInTargetDirectory == "yes")
+            {
+                int result = Helper.DeleteAllFilesInFolder(targetPath);
+                if (result == -1) return;
+            }
+
+            string lastName;
+
+            foreach (var zip in zipFiles)
+            {
+                string filename = Path.GetFileNameWithoutExtension(zip);
+
+                int startIndex = filename.IndexOf("A00");
+                if (startIndex < 0 || startIndex > 16)
+                {
+                    // this represents a group filename
+                    startIndex = filename.IndexOf(" - ");
+                    startIndex += 3;
+                }
+                int endIndex = Helper.GetMonthIndex(filename);
+
+                if (sortBy != null && sortBy == "last-name")
+                {
+                    lastName = Helper.GetLastNameBefore(filename, endIndex);
+                    string dirName = lastName + filename.Substring(startIndex, endIndex - startIndex);
+                    string destination = targetPath + Path.DirectorySeparatorChar + dirName + Path.DirectorySeparatorChar;
+                    try
+                    {
+                        ZipFile.ExtractToDirectory(zip, destination);
+                    }
+                    catch (Exception ex)
+                    {
+                        Helper.Log($"sortBy = last-name\r\n {ex.ToString()}");
+                    }
+                }
+                else
+                {
+                    if (endIndex > -1)
+                    {
+                        string dirName = filename.Substring(startIndex, endIndex - startIndex);
+                        string destination = targetPath + Path.DirectorySeparatorChar + dirName + Path.DirectorySeparatorChar;
+                        try
+                        {
+                            ZipFile.ExtractToDirectory(zip, destination);
+                        }
+                        catch (Exception ex)
+                        {
+                            Helper.Log($"sortBy = student-number\r\n {ex.ToString()}");
+                        }
+                    }
+
+                }
+
+                Console.WriteLine("Extracted {0}", zip);
+            }
+
+            // copy any html files
+            string[] htmlFiles = Directory.GetFiles(sourcePath, "*.html");
+            foreach (var source in htmlFiles)
+            {
+                Helper.CopyFileToTarget(source, targetPath);
+            }
+
+            // copy any xls (Excel) files
+            string[] excelFiles = Directory.GetFiles(sourcePath, "*.xls");
+            foreach (var source in excelFiles)
+            {
+                Helper.CopyFileToTarget(source, targetPath);
+            }
+
+            // copy any txt files
+            string[] txtFiles = Directory.GetFiles(sourcePath, "*.txt");
+            foreach (var source in txtFiles)
+            {
+                Helper.CopyFileToTarget(source, targetPath);
+            }
+        }
+
+    }
+}
